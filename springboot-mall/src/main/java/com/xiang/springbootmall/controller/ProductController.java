bc1aab478c5b61242e7c8baf47cfd4fe5f3e8025
Xiang87878 <winterfoxy11@gmail.com>
2024-07-17 17:27:43 +0800

    商品功能 - 改善參數傳遞

--- springboot-mall/src/main/java/com/xiang/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/xiang/springbootmall/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.xiang.springbootmall.controller;
 
 
 import com.xiang.springbootmall.constant.ProductCategory;
+import com.xiang.springbootmall.dto.ProductQueryParams;
 import com.xiang.springbootmall.dto.ProductRequest;
 import com.xiang.springbootmall.model.Product;
 import com.xiang.springbootmall.service.ProductService;
@@ -24,7 +25,13 @@ public class ProductController {
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search
             ) {
-        List<Product> productList = productService.getProducts(category, search);
+
+        ProductQueryParams productQueryParams = new ProductQueryParams();
+        productQueryParams.setCategory(category);
+        productQueryParams.setSearch(search);
+
+        List<Product> productList = productService.getProducts(productQueryParams);
+
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
 
---
+++ springboot-mall/src/main/java/com/xiang/springbootmall/dto/ProductQueryParams.java
@@ -0,0 +1,26 @@
+package com.xiang.springbootmall.dto;
+
+import com.xiang.springbootmall.constant.ProductCategory;
+
+public class ProductQueryParams {
+
+    private ProductCategory category;
+    private String search;
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+}
