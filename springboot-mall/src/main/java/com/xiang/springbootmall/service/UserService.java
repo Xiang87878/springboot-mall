a3dbc0b43c31a925b1cc2a1ac56ffc31b367b586
Xiang87878 <winterfoxy11@gmail.com>
2024-07-25 10:13:39 +0800

    帳號功能 - 註冊新帳號

---
+++ springboot-mall/src/main/java/com/xiang/springbootmall/service/UserService.java
@@ -0,0 +1,12 @@
+package com.xiang.springbootmall.service;
+
+import com.xiang.springbootmall.dto.UserRegisterRequest;
+import com.xiang.springbootmall.model.User;
+
+public interface UserService {
+
+    User getUserById(Integer userId);
+
+    Integer register(UserRegisterRequest userRegisterRequest);
+
+}
