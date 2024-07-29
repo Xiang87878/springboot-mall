dbd14d74c1c7bb6f34c8a8dd2a7c81b03fa11ddb
Xiang87878 <winterfoxy11@gmail.com>
2024-07-29 18:28:54 +0800

    帳號功能 - 檢查註冊的Email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/xiang/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/xiang/springbootmall/dto/UserRegisterRequest.java
@@ -1,14 +1,17 @@
 package com.xiang.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
     private String password;
+
 
     public String getEmail() {
         return email;
